4ffd7a7743c8947559a1104a5067cb74d80f4c64
Nick Baker <nwb@cap.(none)>
2010-08-15 23:21:34 -0600

    Adding new survey popup helper that will decide when to show the survey popup prompt.

---
+++ views/helpers/survey.php
@@ -0,0 +1,44 @@
+<?php
+App::import('Component', 'Cookie');
+class SurveyHelper extends AppHelper {
+  /**
+    * Load the view object so we can dynamically load an element into it
+    * Load the Cookie Component so we can read from the cookie the CakePHP way
+    */
+  function __construct($settings = array()){
+    $this->View = ClassRegistry::getObject('view');
+  }
+  
+  /**
+    * Setup the cookie component to work in a helper.
+    * we need this so we can write a cookie if we show the popup.
+    */
+  function setupCookie(){
+    $this->Cookie = new CookieComponent();
+    $this->Cookie->initialize(new Object()/* ignored */, array(
+      'time' => '1 Month'
+    )); //init so we can write to the cookie
+    $this->Cookie->startup(); //setup so we can read from the cookie
+  }
+  
+  /**
+    * Decides if the popup should be shown or not
+    * @return boolean
+    */
+  function popupDisplayed(){
+    return !!$this->Cookie->read('Survey.token');
+  }
+  
+  /**
+    * Show the popup element if we haven't displayed it to the user before
+    * @return mixed popup element or null
+    */
+  function showPopup(){
+    if(!$this->popupDisplayed()){
+      $this->Cookie->write('Survey.token', md5(date('Y-m-d H:i:s')));
+      return $this->View->element('survey_popup', array('plugin' => 'survey')); 
+    }
+    return null;
+  }
+}
+?>
