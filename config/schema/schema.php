6d3cdb051481292e8ae01bcff40f3cde3819507d
Nick Baker <nwb@cap.(none)>
2010-09-15 11:16:20 -0600

    Updating schema to match table structure required

--- config/schema/schema.php
+++ config/schema/schema.php
@@ -1,8 +1,8 @@
 <?php 
 /* SVN FILE: $Id$ */
-/* TestApp schema generated on: 2010-08-19 10:08:33 : 1282235913*/
-class TestAppSchema extends CakeSchema {
-	var $name = 'TestApp';
+/* survey schema generated on: 2010-09-15 11:09:41 : 1284570941*/
+class surveySchema extends CakeSchema {
+	var $name = 'survey';
 
 	function before($event = array()) {
 		return true;
@@ -13,26 +13,33 @@ class TestAppSchema extends CakeSchema {
 
 	var $survey_answers = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'question' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50),
-		'answer' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'question' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'answer' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'survey_contact_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
 	var $survey_contacts = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'first_name' => array('type' => 'string', 'null' => true, 'default' => NULL),
-		'last_name' => array('type' => 'string', 'null' => true, 'default' => NULL),
-		'email' => array('type' => 'string', 'null' => false, 'default' => NULL),
-		'phone' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20),
+		'first_name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'last_name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'phone' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'is_18' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'entered_give_away' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'finished_survey' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'final_email_sent_date' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'final_email_sent' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $survey_opt_ins = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
 }
 ?>
